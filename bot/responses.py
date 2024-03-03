6170f1ece8a36a687580b2c8489d497e8099d7a6
gdimmortal <gdimmortaldev@gmail.com>
2024-03-04 01:17:21 +0530

    update constants and responses to use servers from constants rather than hard coding in if condition

--- bot/responses.py
+++ bot/responses.py
@@ -1,10 +1,11 @@
+from constants import DEV_SERVERS
 from time import sleep
 import random
 
 
-def get_response(user_input: str, username: str, server: str, channel: str) -> str:
+def get_response(user_input: str, username: str, server: str, channel: str, server_id: str) -> str:
     lowered: str = user_input.lower()
-    if server != "bots":
+    if not server in DEV_SERVERS.values() or not server_id in DEV_SERVERS.keys():
         return ""
     else:
         if lowered == '':
--- constants.py
+++ constants.py
@@ -7,3 +7,6 @@ load_dotenv()
 
 TOKEN: Final[str] = os.getenv("BOT_TOKEN")
 print(TOKEN)
+
+DEV_SERVERS: set = {"1213920998965907636": "bots", "": "server bookmark"}
+ALPHA_TEST_SERVERS: set = {}
