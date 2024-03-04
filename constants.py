8224cc4447728e7c38e6549d8b47ff7e7d7341e0
gdimmortal <gdimmortaldev@gmail.com>
2024-03-05 00:09:48 +0530

    add alpha test servers and nekoweb api endpoint

--- constants.py
+++ constants.py
@@ -6,7 +6,10 @@ import os
 load_dotenv()
 
 TOKEN: Final[str] = os.getenv("BOT_TOKEN")
-print(TOKEN)
+# print(TOKEN)
 
 DEV_SERVERS: set = {"1213920998965907636": "bots", "": "server bookmark"}
 ALPHA_TEST_SERVERS: set = {}
+
+
+NEKOWEB_INFO_EP = "https://nekoweb.org/api/site/info"
