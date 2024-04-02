e17cfd017ff97ad1073a98628fcd0fb01b0042a8
gdimmortal <gdimmortaldev@gmail.com>
2024-04-03 03:01:02 +0530

    add fields dict map in embeds

--- bot/views/embeds.py
+++ bot/views/embeds.py
@@ -3,19 +3,40 @@ from discord import Message, Embed, Color
 from discord.ext.commands import Context
 from constants import NEKOWEB_INFO_EP, NEKOWEB_SITE_URL, NEKOWEB_SITE_SS
 from datetime import datetime, timezone
+from bot.utils import is_url_exists
 import requests
 
 
-def is_url_exists(url: str) -> bool:
-    r = requests.head(url)
-    return r.status_code == 200
+# TODO: use dictionary to get embed value and opts from name for ex: "id or username or title" as key
+# field_map_key: {name: 'lol', inline: True/False}
+# curf = field_map['field_map_key']
+# embed.add_field(name=curf['name'], value=f"{data['key']}", inline=curf['inline'])
 
+# THIS DICTS KEYS ARE SEQUENCE SENSITIVE
+field_map = {
+"id":          {'name': "", 'inline': True},
+"username":    {'name': "", 'inline': True},
+"title":       {'name': "", 'inline': True},
+"description": {'name': "", 'inline': True}, 
+"note":        {'name': "", 'inline': True}, 
+
+"followers":   {'name': "", 'inline': True},
+"views":       {'name': "", 'inline': True},
+"hits":        {'name': "", 'inline': True},
+"updates":     {'name': "", 'inline': True},
+
+"created_at":  {'name': "", 'inline': True},
+"updated_at":  {'name': "", 'inline': True},
+"domain":      {'name': "", 'inline': True},
+"thumbnail":   {'name': "", 'inline': True},
+}
 
 async def create_site_profile_embed(data: dict, username: str, ctx: Context) -> Embed:
     embed: Embed = Embed(
         title=f"{username}", 
-        description=f"site info of {username} on nekoweb", timestamp=datetime.utcnow(), 
-        color=discord.Color.dark_green()
+        description=f"site info of {username} on nekoweb", timestamp=datetime.now(), 
+        # color=discord.Color.dark_green()
+        color='#'
     )
 
     embed.set_image(url=NEKOWEB_SITE_SS.replace("<site>", username))
@@ -23,18 +44,17 @@ async def create_site_profile_embed(data: dict, username: str, ctx: Context) -> 
     embed.add_field(name="id:", value=f"{data['id']}", inline=True)
     embed.add_field(name="Username:", value=f"{data['username']}", inline=True)
     embed.add_field(name="Title:", value=f"{data['title']}", inline=True)
-    
-    if is_url_exists(config := f'{NEKOWEB_SITE_URL.replace("<site>", username)}/nene/config.json'):
-        config = requests.get(config).json()
-        if 'thumbnail' in config:
-            embed.set_thumbnail(url=config['thumbnail'])
-        if 'description' in config:
-            embed.add_field(name="Description:", value=f"{config['description']}", inline=True)
+
+    # custom config data
+    # if 'thumbnail' in config:
+    #     embed.set_thumbnail(url=config['thumbnail'])
+    # if 'description' in config:
+    #     embed.add_field(name="Description:", value=f"{config['description']}", inline=True)
         
     # embed.add_field(name="\t", value="\t", inline=True)
     
     embed.add_field(name="Followers:", value=f"{data['followers']}", inline=True)
-    embed.add_field(name="Views:", value=f"{data['views']}", inline=True)
+    embed.add_field(name=f"Views:", value=f"{data['views']}", inline=True)
     embed.add_field(name="Updates:", value=f"{data['updates']}", inline=True)
     
     embed.add_field(name="Created at:", 
